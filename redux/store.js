036191899dacd1ce77871857addd70b17734d4f2
johsan32 <hasanulusoy78@hotmail.com>
2024-03-22 23:15:18 +0300

    yeni

--- redux/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import contentSlice from './favoritesReducer';
-
-export const store = configureStore({
-  reducer: {
-    content: contentSlice,
-  },
-});
