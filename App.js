a726b95dcc42b3fda54d807df68d9c95d76d8fb3
ozbuganliramazan <ozbuganliramazan@outlook.com>
2024-03-25 03:42:23 +0300

    redux toolkit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/router/rootNavigator';
+import {Provider} from 'react-redux';
+import store from './src/store/store';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <RootNavigator />
+      <Provider store={store}>
+        <RootNavigator />
+      </Provider>
     </NavigationContainer>
   );
 }
--- src/components/characters /charactersCard.js
+++ src/components/characters /charactersCard.js
@@ -1,21 +1,64 @@
-import {View, Text, ImageBackground, TouchableOpacity} from 'react-native';
-import React from 'react';
-import {resizeModes, width} from '../../utils/constans';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+} from 'react-native'; // Alert ekledik
 import {characteCardStyles} from '../../styles/characteCardStyles';
-import {characterScreenStyles} from '../../styles/characters.styles';
-import {AppColors} from '../../theme/colors';
+import {resizeModes} from '../../utils/constans';
 import {CHARACTERDETAIL} from '../../utils/routes';
 import {useNavigation} from '@react-navigation/native';
-const CharactersCard = props => {
-  const {item} = props;
-  const navigation = useNavigation();
+import {useDispatch, useSelector} from 'react-redux';
+import {addFavorite, removeFavorite} from '../../store/favoritesSlice';
+
+const FavoriteButton = ({item}) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector(state => state.favorites.favorites);
+
+  const isFavorite = favorites.some(fav => fav.id === item.id);
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      dispatch(removeFavorite(item));
+      Alert.alert('Favori Çıkarıldı');
+    } else {
+      if (favorites.length < 10) {
+        dispatch(addFavorite(item));
+        Alert.alert('Favori Eklendi');
+      } else {
+        Alert.alert('Favori Ekleme Sınırına Ulaşıldı');
+      }
+    }
+  };
+
   return (
     <TouchableOpacity
-      onPress={() => navigation.navigate(CHARACTERDETAIL, {item: item})}>
+      onPress={toggleFavorite}
+      style={characteCardStyles.favoriteButton}>
+      <Text
+        style={[
+          characteCardStyles.favoriteIcon,
+          {color: isFavorite ? 'red' : 'white'},
+        ]}>
+        {isFavorite ? '❤️' : '🤍'}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const CharactersCard = ({item}) => {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.navigate(CHARACTERDETAIL, {item})}>
       <ImageBackground
         source={{uri: item.image}}
         resizeMode={resizeModes.CONTAIN}
         style={characteCardStyles.container}>
+        <FavoriteButton item={item} />
         <View style={characteCardStyles.infoContainer}>
           <Text numberOfLines={1} style={characteCardStyles.name}>
             {item.name}
